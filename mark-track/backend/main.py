44c3e3466f218848d55f69be557ef7db461f759c
Raebschi Catalin <catalin.raebschi@ctif.gov.md>
2024-10-24 16:18:22 +0300

    initial setup next+fastapi+folder system

---
+++ mark-track/backend/main.py
@@ -0,0 +1,13 @@
+# backend/main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Mark-Track API"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
