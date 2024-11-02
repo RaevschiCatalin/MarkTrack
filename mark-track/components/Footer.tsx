1d2f45150e8efafbce6a981c54e6ea36c441d234
GORTHMAN1 <gabriel.feher03@e-uvt.ro>
2024-11-02 17:57:42 +0200

    fixed marktrack spelling

--- mark-track/components/Footer.tsx
+++ mark-track/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="">
         <div className="flex justify-center items-center h-20">
-            <p className="text-sm">MarkTrack © 2024</p>
+            <p className="text-sm">marktrack © 2024</p>
         </div>
     </footer>
   )
