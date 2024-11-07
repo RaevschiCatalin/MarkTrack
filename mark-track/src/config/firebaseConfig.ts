bd79da7bd42d4772c48df8bf5810b87996c26465
RaevschiCatalin <raevschicatalin@gmail.com>
2024-11-07 14:33:11 +0200

    deploy.yml modified to run only on build

--- mark-track/src/config/firebaseConfig.ts
+++ mark-track/src/config/firebaseConfig.ts
@@ -1,14 +1,18 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import dotenv from 'dotenv';
-import path from 'path';
 import {getFirestore} from "@firebase/firestore";
-import { getDatabase } from 'firebase/database';
 
-// Load environment variables from .env file
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
+
+console.log("Firebase Config:", {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+});
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
