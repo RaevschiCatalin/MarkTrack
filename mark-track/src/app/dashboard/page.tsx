4e3034044fa6a936c5055f895694f23484255f9c
GORTHMAN1 <gabriel.feher03@e-uvt.ro>
2024-11-16 17:47:47 +0200

    Navbar changes and teacher dashboard basic sidebar

--- mark-track/src/app/dashboard/page.tsx
+++ mark-track/src/app/dashboard/page.tsx
@@ -8,15 +8,15 @@ export default function Dashboard() {
             <label htmlFor="sidebar-mobile-fixed" className="sidebar-overlay"></label>
             <aside className="sidebar sidebar-sticky sidebar-mobile h-full max-sm:fixed max-sm:-translate-x-full py-4">
                 <section className="sidebar-title items-center p-4">
-                    <span className="text-success text-xl text-pretty">Dashboard</span>
+                    <h1 className="text-warning text-pretty text-xl">Dashboard</h1>
                 </section>
                 <section className="sidebar-content">
                     <nav className="menu rounded-md">
                         <section className="menu-section px-4">
                             <ul className="menu-items">
-                                <li className="menu-item text-primary">Class Name</li>
-                                <li className="menu-item text-primary">Class Name</li>
-                                <li className="menu-item text-primary">Class Name</li>
+                                <li className="menu-item text-primary text-base">Class Name</li>
+                                <li className="menu-item text-primary text-base">Class Name</li>
+                                <li className="menu-item text-primary text-base">Class Name</li>
                             </ul>
                         </section>
                     </nav>
@@ -32,9 +32,6 @@ export default function Dashboard() {
 
 		        <div className="my-4 grid grid-rows-1 gap-4">
 			        <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-                    <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-                    <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-                    <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
                     <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
                     <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
                 </div>
